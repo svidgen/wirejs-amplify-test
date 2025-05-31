b2036ca21c415827c207b3f506e5c7199c0fa7fd
Jon Wire <iambipedal@gmail.com>
2025-05-31 12:31:18 -0500

    switch wjs deps back to latest branch

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,8 @@ import { Chat } from './apps/chat.js';
 import { Todos } from './apps/todos.js';
 import { Wiki } from './apps/wiki.js';
 
+export type { Todo } from './apps/todos.js';
+
 const authService = new AuthenticationService('app', 'core-users');
 
 export const auth = authService.buildApi();
